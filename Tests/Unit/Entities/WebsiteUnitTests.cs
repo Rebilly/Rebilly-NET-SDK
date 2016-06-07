0efa89144e5d6cac7ee67a4ed586c1d2cc789dff
Ryan McFadden <ryanpmcfadden@gmail.com>
2016-06-07 09:01:21 -0700

    Trigger first test build.

--- Tests/Unit/Entities/WebsiteUnitTests.cs
+++ Tests/Unit/Entities/WebsiteUnitTests.cs
@@ -28,6 +28,57 @@ namespace Tests.Unit.Entities
             var Website = new Website();
             Website.Id = "test1";
             Assert.AreEqual("test1", Website.Id);
-        }  
+        }
+
+
+        [Test]
+        public void TestNameDefaultIsEqualTo()
+        {
+            var Website = new Website();
+            Assert.IsNull(Website.Name);
+        }
+
+
+        [Test]
+        public void TestNameIsEqualTo()
+        {
+            var Website = new Website();
+            Website.Name = "test2";
+            Assert.AreEqual("test2", Website.Name);
+        }
+
+
+        [Test]
+        public void TestUrlDefaultIsEqualTo()
+        {
+            var Website = new Website();
+            Assert.IsNull(Website.Url);
+        }
+
+
+        [Test]
+        public void TestUrlIsEqualTo()
+        {
+            var Website = new Website();
+            Website.Url = "testing.com";
+            Assert.AreEqual("testing.com", Website.Url);
+        }
+
+
+        [Test]
+        public void TestServicePhoneDefaultIsEqualTo()
+        {
+            var Website = new Website();
+            Assert.IsNull(Website.ServicePhone);
+        }
+
+
+        [Test]
+        public void TestServicePhoneIsEqualTo()
+        {
+            var Website = new Website();
+            Website.ServicePhone = "802-234-2344";
+            Assert.AreEqual("802-234-2344", Website.ServicePhone);
+        }
     }
 }
